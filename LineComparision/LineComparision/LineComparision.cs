d9294cefe2d2696abbae25ea3c2adfe46ab4d229
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-13 23:05:51 +0530

    [pavan] Checking Lengths Equal or Not

--- LineComparision/LineComparision/LineComparision.cs
+++ LineComparision/LineComparision/LineComparision.cs
@@ -8,7 +8,8 @@ namespace LineComparision
 {
     public class LineOne
     {
-        public double Length1()
+
+        public double Length()
         {
             Console.WriteLine("Enter the Value of X1 for Cal Length1");
             int x1 = Convert.ToInt32(Console.ReadLine());
@@ -18,9 +19,10 @@ namespace LineComparision
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the Value of Y2 for Cal Length1");
             int y2 = Convert.ToInt32(Console.ReadLine());
+
             double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length of the Line1:  " + result);
             return result;
         }
     }
+      
 }
--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -2,4 +2,15 @@
 
 Console.WriteLine("Welcome to Line Comparision: ");
 LineOne line = new LineOne();
-Console.WriteLine(line.Length1()); 
+double result1 = line.Length();
+Console.WriteLine("Length Of Line1: {0}",result1);
+double result2 = line.Length();
+Console.WriteLine("Length Of Line2: {0}", result2);
+
+if (result1 == result2)
+    Console.WriteLine("Length1 and length2  Are Equal");
+else
+    Console.WriteLine("Length1 and length2  Are not Equal");
+
+
+
